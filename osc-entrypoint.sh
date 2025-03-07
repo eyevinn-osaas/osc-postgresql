f0ace642e67486f70dc5bf9b5cab779c8f1cea66
Jonas Birmé <jonas.birme@eyevinn.se>
2025-03-07 23:11:01 +0100

    feat: can run sql on init

--- osc-entrypoint.sh
+++ osc-entrypoint.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 mkdir -p /tmp/health
 python3 -m http.server 8080 --directory /tmp/health &
+
+if [[ ! -z "$POSTGRES_INITDB_SQL" ]]; then
+  mkdir /docker-entrypoint-initdb.d/
+  echo "$POSTGRES_INITDB_SQL" > /docker-entrypoint-initdb.d/init.sql
+fi
 
 # Run the original entrypoint script
 /usr/local/bin/docker-entrypoint.sh "$@"
